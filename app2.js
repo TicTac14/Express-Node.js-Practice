ae10ad9946075fe24478d76dc68bb8f81bddda31
TicTac14 <francisco.mendez114@gmail.com>
2023-10-27 21:57:47 -0400

    new feat

---
+++ app2.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// setup static and middleware
+app.use(express.static('./navbar-app')); // serve static files
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, './navbar-app/index.html'));
+})
+
+app.get('/about', (req, res) => {
+    res.status(200).send('About Page');
+})
+
+app.all('*', (req, res) => { // handling 404
+    res.status(404).send('Cannot Find Page :(')
+})
+
+app.listen(3000, () => {
+    console.log('Server Listening on port 3000...');
+})
